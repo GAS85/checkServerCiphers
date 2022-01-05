4a493c8c8a04fca3cf3268d91274271c2b0ce113
Georgiy Sitnikov <Georgiy.Sitnikov@telekom.de>
2022-01-05 10:27:13 +0100

    Move to curl Add user Ciphers list definition redesign Add Comments Update Readme

--- checkServerCiphers.sh
+++ checkServerCiphers.sh
@@ -4,30 +4,35 @@
 #
 # AS-IS without any warranty
 
-# Protocols to test, actually ssl3 is not supported anymore
-protocols="ssl3 tls1 tls1_1 tls1_2 tls1_3"
-# Timeout in seconds, put e.g. 2 or 5 on slower servers / connections
-timeout=1
+# TLS Protocols to test
+protocols="1.1 1.2 1.3"
+# Timeout in seconds, put e.g. 5 on slower servers / connections
+timeout=2
 
 while test $# -gt 0; do
 	case "$1" in
-		--help)
-			echo "Simple Server Chiphers Scanner based on openssl."
+		-h,--help)
+			echo "Simple Server Chiphers Scanner based on openssl and curl."
 			echo ""
-			echo "Execution:"
-			echo "checkServerCiphers.sh <host>:<port> <parameter>"
+			echo "Usage: ./checkServerCiphers.sh <protocol>://<host>:<port> [options] [ciphers]"
+			echo "Example:"
+			echo "  ./checkServerCiphers.sh https://myserver.com:8080"
+			echo "  ./checkServerCiphers.sh https://myserver.com -v ALL:eNULL"
 			echo ""
-			echo "  -v        verbose Protocol output."
-			echo "  -vv       verbose Ciphers output."
-			echo "  -vvv      verbose OpenSSL output.
-"
-			echo "  -s        Test only supported Ciphers
-            this is defalut setting.
-"
-			echo "  -all      Test all (also unsupported) Ciphers."
-			echo "  -allNull  Test all Ciphers (also unsupported),
-            even with zero encryption.
-"
+			echo "  <protocol>   Could be HTTPS, FTPS, IMAPS, SMTPS, etc."
+			echo "  <host>	   FQDN that you need to test"
+			echo "  <port>	   Port that you need to test"
+			echo ""
+			echo "Options:"
+			echo "  -v		   verbose Protocol output."
+			echo "  -vv		  verbose Ciphers output."
+			echo "  -vvv		 verbose Curl output."
+			echo ""
+			echo "Ciphers:"
+			echo "  ALL		  Test ALL Ciphers, this is defalut setting."
+			echo "  ALL:eNULL	Test also Ciphers with zero encryption."
+			echo "  HIGH:MEDIUM  etc. Please refer to https://curl.se/docs/ssl-ciphers.html
+			   for more information."
 			exit 0
 			;;
 		*)
@@ -36,11 +41,24 @@ while test $# -gt 0; do
 	esac
 done
 
-# Connection Test
+# Set different Variables
+linkToTest=$1
 
-if [ "$(curl -skL -m $timeout -w "%{http_code}\n" https://$1 -o /dev/null)" -eq 000 ]; then
+[[ $2 == "-v" || $2 == "-vv" || $2 == "-vvv" ]] && { verboseLvL="$(echo $2 | wc -m)" ; cipherString=$3 ; } || { verboseLvL=0 ; cipherString=$2 ; }
 
-	echo "ERROR - Could not connect to: https://$1"
+[[ "$verboseLvL" == "5" ]] && { curlOptions="-kl" ; } || { curlOptions="-skl -o /dev/null"; }
+
+[[ -z $cipherString ]] && { cipherString="ALL" ; }
+
+curlTLSOptions="--ciphers"
+
+found="Results for \t$linkToTest"
+
+# Connectivity Test
+
+if [ "$(curl $curlOptions -m $timeout -w "%{http_code}\n" --ssl-reqd $linkToTest)" -eq 000 ]; then
+
+	echo "ERROR - Could not connect to: $linkToTest"
 
 	exit 1
 
@@ -48,49 +66,34 @@ fi
 
 opensslCiphers () {
 
-	#openssl ciphers 'ALL:eNULL' | tr ':' ' '
-
-	# Test only Supported Ciphers
-	[[ $1 == "" || $1 == "-s" || $2 == "-s" ]] && { ciphersList="$(openssl ciphers -s -$protocol 2>/dev/null | tr ':' ' ')" ; }
-
-	# Test All Ciphers
-	[[ $1 == "-all" || $2 == "-all" ]] && { ciphersList="$(openssl ciphers -$protocol 2>/dev/null | tr ':' ' ')" ; }
-
-	# Test All Ciphers with zero encryption
-	[[ $1 == "-allNull" || $2 == "-allNull" ]] && { ciphersList="$(openssl ciphers 'ALL:eNULL' 2>/dev/null | tr ':' ' ')" ; }
+	# Get list of Ciphers based on User input
+	ciphersList="$(openssl ciphers $cipherString 2>/dev/null | tr ':' ' ')"
 
 }
 
-echo -e "Now scanning:\t$1"
-
-found="Results for $1:"
+echo -e "Now scanning:\t$linkToTest with '$cipherString' set of Ciphers"
 
 # TLS Scan
 for protocol in $protocols; do
 
-	[[ $2 == "-v" || $2 == "-vv" || $2 == "-vvv" ]] && { echo -e "Checking:\t$protocol ..."; }
+	[[ "$verboseLvL" -ge 3 ]] && { echo -e "Checking:\tTLSv$protocol ..."; }
 
-#	for cipher in $(openssl ciphers 'ALL:eNULL' | tr ':' ' '); do
+	opensslCiphers
 
-	opensslCiphers $2 $3
-
-	[[ $2 == "-vv" || $2 == "-vvv" ]] && { echo -e "Following Ciphers will be tested:\t$ciphersList"; }
+	[[ "$verboseLvL" -ge 4 ]] && { echo -e "Following Ciphers will be tested:\t$ciphersList"; }
 
 	for cipher in $ciphersList; do
 
-		[[ $2 == "-vv" || $2 == "-vvv" ]] && { echo -e "Checking:\t$protocol with Cipher:\t$cipher"; }
+		[[ "$verboseLvL" -ge 4 ]] && { echo -e "Checking:\ttTLSv$protocol with Cipher:\t$cipher"; }
 
-		if [[ $2 == "-vvv" ]]; then
+		# change Options Syntax when TLS 1.3 being used, otherwise will accept all because of Server negotioation
+		[[ "$protocol" == "1.3" ]] && { curlTLSOptions="--tls13-ciphers" ; }
 
-			timeout $timeout openssl s_client -connect $1 -cipher $cipher -$protocol < /dev/null && \
-			found="${found}\n$(echo -e "Protocol:\t$protocol with Cipher:\t$cipher")" && \
-			echo -e "Found:    \t$protocol with Cipher:\t$cipher"
+		if [ "$(curl $curlOptions -m $timeout -w "%{http_code}\n" $linkToTest --tlsv$protocol --tls-max $protocol $curlTLSOptions $cipher)" -gt 000 ]; then
 
-		else
+			found="${found}\n$(echo -e "Protocol:\tTLSv$protocol with Cipher:\t$cipher")"
 
-			timeout $timeout openssl s_client -connect $1 -cipher $cipher -$protocol < /dev/null > /dev/null 2>&1 && \
-			found="${found}\n$(echo -e "Protocol:\t$protocol with Cipher:\t$cipher")" && \
-			[[ $2 == "-vv" || $2 == "-vvv" ]] && { echo -e "Found:    \t$protocol with Cipher:\t$cipher"; }
+			[[ "$verboseLvL" -ge 3 ]] && { echo -e "Found:	\tTLSv$protocol with Cipher:\t$cipher" ; }
 
 		fi
 
@@ -98,6 +101,7 @@ for protocol in $protocols; do
 
 done
 
+# Show Results
 echo -e "${found}"
 
 exit 0
